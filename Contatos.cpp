bd4923e7b33c827b1f854b376754812fd987b527
Ketleen Anne <ketleen@ice.ufjf.br>
2021-06-16 15:40:07 -0300

    classes novas criadas

---
+++ Contatos.cpp
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Contato{
+    public:
+        Contato();
+        ~Contato();
+
+    private:
+        char[] name;
+        int idade;
+        int telefone[];
+
+}
+
